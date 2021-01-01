e67004afb1571f8ab1d31e85e9cdba311ab7d202
unknown <suhyeon7846@gmail.com>
2021-01-01 16:22:28 +0900

    noiceserviceI modified

--- src/com/teum/service/NoticeServiceI.java
+++ src/com/teum/service/NoticeServiceI.java
@@ -2,13 +2,24 @@ package com.teum.service;
 
 import java.util.List;
 
+import com.teum.dao.entity.NoticeView;
 import com.teum.entity.Notice;
 
 public interface NoticeServiceI {
 
-	List<Notice> getList(int id, String search);
+	List<Notice> getList(int page,String query);
 	
-	int openAll(int[] ids);
+	List<NoticeView> getViewList(int page,String query);
+	
+	Notice get(int id);
+	
+	Notice getLast();
+
+	int getCount(String query);
+	
+	int openAll(String oIdsCSV, String cIdsCSV);
+	
+	int closeAll(String oIdsCSV, String cIdsCSV);
 	
 	int deleteAll(int[] ids);
 	
